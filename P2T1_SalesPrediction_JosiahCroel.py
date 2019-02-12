31e60f7807e59d3f60b13df3fa844c2129135867
croelj <47565809+croelj@users.noreply.github.com>
2019-02-12 09:25:51 -0500

    First Submission

---
+++ P2T1_SalesPrediction_JosiahCroel.py
@@ -0,0 +1,14 @@
+# Sales Prediction 
+# February 5, 2019
+# CTI-110 P2T1 - Sales Prediction
+# Josiah Croel
+#
+
+#Get projected total sales.
+total_sales = float( input ('Enter the projected sales: '))
+
+#Calculate the profit as 23% of total sales.
+profit = total_sales * 0.23
+
+#Display the Profit
+print('The profit is $', format (profit, ',.2f')) 
